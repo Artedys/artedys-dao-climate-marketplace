282f032d022099c35da3e17a7d43346382486941
ARTEDYS <Artedys@users.noreply.github.com>
2022-11-29 19:55:08 +0100

    Update 2-deploy-drop.js

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,5 +1,3 @@
-
-
 import { AddressZero } from "@ethersproject/constants";
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
@@ -8,11 +6,12 @@ import { readFileSync } from "fs";
   try {
     const editionDropAddress = await sdk.deployer.deployEditionDrop({
       // The collection's name, ex. CryptoPunks
-      name: "ARTEDYX GREEN BLOCKCHAIN CLIMATE MARKETPLACE DAO Membership",
+      name: "ARTEDYX DAO CLIMATE Marketplace New Stock Exchange Membership",
       // A description for the collection.
-      description: "A DAO for the Consortium members of ARTEDYX GREEN BLOCKCHAIN CLIMATE MARKETPLACE.",
+      description: "A DAO for fans of eSpatiale ARTEDYX DAO CLIMATE Marketplace New Stock Exchange.",
       // The image that will be held on our NFT! The fun part :).
       image: readFileSync("scripts/assets/ARTADYS.jpg"),
+      //image: readFileSync("scripts/assets/Mustapha.png"),
       // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the contract.
       // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
       // you can set this to your own wallet address if you want to charge for the drop.
@@ -21,7 +20,7 @@ import { readFileSync } from "fs";
 
     // this initialization returns the address of our contract
     // we use this to initialize the contract on the thirdweb sdk
-    const editionDrop = await sdk.getEditionDrop(editionDropAddress);
+    const editionDrop = await sdk.getContract(editionDropAddress, "edition-drop");
 
     // with this, we can get the metadata of our contract
     const metadata = await editionDrop.metadata.get();
